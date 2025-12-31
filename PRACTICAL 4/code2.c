a47e524899c1285a56c5a793a4148e1601d3aff3
anshumantiwari2702-lang <anshumantiwari2702@gmail.com>
2025-12-31 22:24:14 +0530

    Update code2.c

--- PRACTICAL 4/code2.c
+++ PRACTICAL 4/code2.c
@@ -6,7 +6,7 @@ int main()
     //compile time format specifiers
     int a;
 printf("ANSHUMAN TIWARI\n");
-    printf("ERP TD-RU-25-10221\n");
+    printf("ERP TD-RU-25-10263\n");
     printf("enter a\n");
     printf("%f",&a);
     printf("print number:%d",a);
